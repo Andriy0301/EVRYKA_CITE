8161b903af7079b9aa091374051730dd82091d33
Andriy0301 <andrysejko22@email.com>
2026-04-07 13:30:39 +0300

    fix port

--- server/server.js
+++ server/server.js
@@ -27,6 +27,8 @@ app.use("/popularity", popularityRoute);
 app.use("/images", express.static(path.join(__dirname, "../client/images")));
 
 // 🚀 Запуск сервера
-app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
